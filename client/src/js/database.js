c46e0e95ef2a8fa94e58eb8e16d59c8a1215608e
ChapstickLover97 <ashtonbalder@gmail.com>
2023-05-25 23:21:28 -0500

    More adjustments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const initdb = async () => {
+  const db = await openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -12,10 +12,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  return db;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const id = await store.put({ content });
+  await transaction.complete;
+  console.log(`Content added to database with ID: ${id}`);
+};
+
+export const getDb = async () => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const content = await store.getAll();
+  await transaction.complete;
+  console.log('Retrieved content from database:', content);
+  return content;
+};
 
 initdb();
